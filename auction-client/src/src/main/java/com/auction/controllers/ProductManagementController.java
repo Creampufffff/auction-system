b52464e3a4cddd713774838abee5e915c05fb329
NgoQuocThang <25020400@vnu.edu.vn>
2026-05-04 20:38:59 +0700

    GUI 1st ver

---
+++ auction-client/src/src/main/java/com/auction/controllers/ProductManagementController.java
@@ -0,0 +1,13 @@
+package com.auction.controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ProductManagementController {
+    @FXML private TableView<?> myProductsTable;
+
+    @FXML public void initialize() {}
+    @FXML private void handleAddProduct(ActionEvent event) {}
+    @FXML private void handleEditProduct(ActionEvent event) {}
+    @FXML private void handleDeleteProduct(ActionEvent event) {}
+}
